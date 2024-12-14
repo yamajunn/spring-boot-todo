5f77a1c7f29c0165f43af9891c7285a75bfc3612
yamajunn <amiton1115@gmail.com>
2024-12-14 11:32:29 +0900

    add: 基底EntityのBaseEntityを作成

--- spring/src/main/java/com/example/todo/entity/Task.java
+++ spring/src/main/java/com/example/todo/entity/Task.java
@@ -8,15 +8,15 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
 @Entity
 @Table(name = "tasks")
-public class Task {
+@EqualsAndHashCode(callSuper = false) // 親クラスの equals, hashCode を使わない（これを設定しないと警告が出る）
+public class Task extends BaseEntity {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -49,28 +49,4 @@ public class Task {
 
   @Column(name = "completed_at")
   private LocalDateTime completedAt;
-
-  @Column(name = "created_at", nullable = false)
-  private LocalDateTime createdAt;
-
-  @Column(name = "updated_at", nullable = false)
-  private LocalDateTime updatedAt;
-
-  /**
-   * @PrePersist JPA（Java Persistence API）のアノテーション。エンティティの新規作成処理が行われる前に呼び出される。
-   */
-  @PrePersist
-  private void onCreate() {
-    LocalDateTime now = LocalDateTime.now();
-    this.setCreatedAt(now);
-    this.setUpdatedAt(now);
-  }
-
-  /**
-   * @PreUpdate JPA（Java Persistence API）のアノテーション。エンティティの更新処理が行われる前に呼び出される。
-   */
-  @PreUpdate
-  private void onUpdate() {
-    this.setUpdatedAt(LocalDateTime.now());
-  }
 }
