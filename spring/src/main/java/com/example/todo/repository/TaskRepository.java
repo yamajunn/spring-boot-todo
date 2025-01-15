448f40ed2cba936adcb3d8fac479e1db6e5e621c
yamajunn <amiton1115@gmail.com>
2025-01-15 13:22:13 +0900

    update: Specification を利用し、検索条件と並び順を制御できるように実装

--- spring/src/main/java/com/example/todo/repository/TaskRepository.java
+++ spring/src/main/java/com/example/todo/repository/TaskRepository.java
@@ -1,7 +1,9 @@
 package com.example.todo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import com.example.todo.entity.Task;
 
-public interface TaskRepository extends JpaRepository<Task, Integer> {
+public interface TaskRepository
+    extends JpaRepository<Task, Integer>, JpaSpecificationExecutor<Task> {
 }
--- spring/src/main/java/com/example/todo/service/tasks/TaskSearchService.java
+++ spring/src/main/java/com/example/todo/service/tasks/TaskSearchService.java
@@ -2,11 +2,14 @@ package com.example.todo.service.tasks;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import com.example.todo.dto.request.tasks.TaskSearchRequest;
 import com.example.todo.dto.response.tasks.TaskBaseResponse;
 import com.example.todo.entity.Task;
 import com.example.todo.repository.TaskRepository;
+import com.example.todo.repository.specification.TaskSpecification;
 
 @Service
 public class TaskSearchService {
@@ -18,9 +21,16 @@ public class TaskSearchService {
   }
 
   public List<TaskBaseResponse> invoke(TaskSearchRequest request) {
-    // TODO: 以下の処理だと、全てのタスクを取得してしまう。
-    // |___| 本来は、リクエストパラメータに応じて取得するタスクを絞り込む必要がある。
-    List<Task> tasks = this.taskRepository.findAll();
+    TaskSpecification taskSpec = new TaskSpecification();
+    Specification<Task> spec =
+        Specification.where(taskSpec.projectIdEquals(request.getProjectId()));
+
+    // completedAt降順 > deadlineAt昇順 > id降順 の並びになるようにソート条件を生成
+    Sort sort = Sort.by(Sort.Order.desc("completedAt"), Sort.Order.asc("deadlineAt"),
+        Sort.Order.desc("id"));
+
+    // 引数に検索条件を指定することで、条件に合致する Task Entity のリストを取得する
+    List<Task> tasks = this.taskRepository.findAll(spec, sort);
 
     // Task Entity のリストを、TaskBaseResponse のリストに変換して返す
     // .collect(Collectors.toList()) は、Stream の要素を List に変換するメソッド
