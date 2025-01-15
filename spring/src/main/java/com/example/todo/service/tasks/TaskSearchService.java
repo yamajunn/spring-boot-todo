c9ad14e6cbbfa00f3cc6193b54617dafd65d1eab
yamajunn <amiton1115@gmail.com>
2025-01-15 12:17:23 +0900

    add: Controllerの実装
      - Service も定義のみ実装

---
+++ spring/src/main/java/com/example/todo/service/tasks/TaskSearchService.java
@@ -0,0 +1,14 @@
+package com.example.todo.service.tasks;
+
+import java.util.List;
+import org.springframework.stereotype.Service;
+import com.example.todo.dto.request.tasks.TaskSearchRequest;
+import com.example.todo.dto.response.tasks.TaskBaseResponse;
+
+@Service
+public class TaskSearchService {
+
+  public List<TaskBaseResponse> invoke(TaskSearchRequest request) {
+
+  }
+}
