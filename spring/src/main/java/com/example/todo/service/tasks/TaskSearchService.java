e2dea070dd59211246e4aed806989fe4fda978bf
yamajunn <amiton1115@gmail.com>
2025-01-15 12:39:35 +0900

    update: Service で データ取得処理を実装

--- spring/src/main/java/com/example/todo/service/tasks/TaskSearchService.java
+++ spring/src/main/java/com/example/todo/service/tasks/TaskSearchService.java
@@ -1,14 +1,29 @@
 package com.example.todo.service.tasks;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
 import com.example.todo.dto.request.tasks.TaskSearchRequest;
 import com.example.todo.dto.response.tasks.TaskBaseResponse;
+import com.example.todo.entity.Task;
+import com.example.todo.repository.TaskRepository;
 
 @Service
 public class TaskSearchService {
 
-  public List<TaskBaseResponse> invoke(TaskSearchRequest request) {
+  private final TaskRepository taskRepository;
 
+  public TaskSearchService(TaskRepository taskRepository) {
+    this.taskRepository = taskRepository;
+  }
+
+  public List<TaskBaseResponse> invoke(TaskSearchRequest request) {
+    // TODO: 以下の処理だと、全てのタスクを取得してしまう。
+    // |___| 本来は、リクエストパラメータに応じて取得するタスクを絞り込む必要がある。
+    List<Task> tasks = this.taskRepository.findAll();
+
+    // Task Entity のリストを、TaskBaseResponse のリストに変換して返す
+    // .collect(Collectors.toList()) は、Stream の要素を List に変換するメソッド
+    return tasks.stream().map(task -> new TaskBaseResponse(task)).collect(Collectors.toList());
   }
 }
